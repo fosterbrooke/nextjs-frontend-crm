05247e6c2395088ae944d587fc755bd950bed604
okisahandsome <okisahandsome@outlook.com>
2023-11-29 04:34:13 -0800

    change 'SAVE & EXIT' button style

--- components/ui/auth/client/SaveExitBtn.tsx
+++ components/ui/auth/client/SaveExitBtn.tsx
@@ -1,24 +1,21 @@
-'use client'
+"use client";
 
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
-const SaveExitBtn = ({
-    onClicked,
-}: {
-    onClicked: Function
-}) => {
-    return (
-        <button
-            type="submit"
-            className="block bg-white hover:bg-primaryHover focus:bg-primaryDisabled 
+const SaveExitBtn = ({ onClicked }: { onClicked: Function }) => {
+  return (
+    <button
+      type="submit"
+      className="block bg-white hover:bg-primaryHover hover:text-white focus:bg-primaryDisabled 
                     text-primary rounded-lg px-4 py-3 mt-4 mb-9 text-loginBtnTextSize font-arial border-2 border-primary"
-            onClick={() => onClicked()}>
-                SAVE & EXIT
-        </button>
-    );
+      onClick={() => onClicked()}
+    >
+      SAVE & EXIT
+    </button>
+  );
 };
 
-SaveExitBtn.displayName = 'SaveExitBtn';
+SaveExitBtn.displayName = "SaveExitBtn";
 
 export default SaveExitBtn;
